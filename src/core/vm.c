cf85829506683b0e9ee994f565524fa3079d5dea
Diogo Costa <diogoandreveigacosta@gmail.com>
2024-02-29 15:42:20 +0000

    feat(event_ex): enable smmu pmu events export
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- src/core/vm.c
+++ src/core/vm.c
@@ -277,10 +277,11 @@ struct vm* vm_init(struct vm_allocation* vm_alloc, const struct vm_config* confi
         #define SMMU_TBU4_CNTR_GROUP    4
         #define SMMU_TBU5_CNTR_GROUP    5
 
-        smmu_pmu_init(SMMU_TBU2_CNTR_GROUP);
+        ssize_t ctx_id = vm->io.prot.mmu.ctx_id;
+        smmu_pmu_init(SMMU_TBU2_CNTR_GROUP, ctx_id);
 
-        size_t event = 0x0010;
-        smmu_pmu_event_add(SMMU_TBU2_CNTR_GROUP, event);
+        // size_t event = 0x0010;
+        // smmu_pmu_event_add(SMMU_TBU2_CNTR_GROUP, event);
 
     }
 
